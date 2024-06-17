8149782725d58dddf2bd33d0f7185246e3acf9f4
Diwakarthedeveloper <djha4635@gmail.com>
2024-06-17 18:04:09 +0530

    changed in plan

--- Winvm.tf
+++ Winvm.tf
@@ -31,7 +31,7 @@ resource "azurerm_network_interface" "djnetint" {
   location            = azurerm_resource_group.djgrp.location
   resource_group_name = azurerm_resource_group.djgrp.name
 
-  
+
 
   ip_configuration {
     name                          = "djinternalip"
