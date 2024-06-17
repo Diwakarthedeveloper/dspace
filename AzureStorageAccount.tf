02ca640d1c1e0b3f34e6c9053c50e1a1476438e6
Diwakarthedeveloper <djha4635@gmail.com>
2024-06-17 12:24:47 +0530

    storage account name changed

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "djgrp" { // resource type - azurerm_resource_
 
 ##  Demo now
 resource "azurerm_storage_account" "DjStorageAccount" {
-  name                     = "Djstore"
+  name                     = "djstore"
   resource_group_name      = azurerm_resource_group.djgrp.name
   location                 = azurerm_resource_group.djgrp.location
   account_tier             = "Standard"
