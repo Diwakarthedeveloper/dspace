f3e6a1ade1d4d40019ec6d1a49b510f86efc162c
Diwakarthedeveloper <djha4635@gmail.com>
2024-06-17 19:08:02 +0530

    ku       = "2019-Datacenter"

--- Winvm.tf
+++ Winvm.tf
@@ -100,7 +100,7 @@ resource "azurerm_windows_virtual_machine" "djwvm" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
 
