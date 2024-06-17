913d7c77af7f637ef9ed3e31fea4730e6fadfd8e
Diwakarthedeveloper <djha4635@gmail.com>
2024-06-17 14:16:18 +0530

    keyvault connect

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "djgrp" { // resource type - azurerm_resource_
   location = "Central India"
 }
 
-##  Demo now
+#Below creating storage account
 resource "azurerm_storage_account" "DjStorageAccount" {
   name                     = "djstore"
   resource_group_name      = azurerm_resource_group.djgrp.name
---
+++ Winvm.tf
@@ -0,0 +1,79 @@
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "djgrp" { // resource type - azurerm_resource_group, local name - djgrp
+  name     = "dj-resources"
+  location = "Central India"
+}
+
+resource "azurerm_virtual_network" "djnet" {
+  name                = "dj-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.djgrp.location
+  resource_group_name = azurerm_resource_group.djgrp.name
+}
+
+resource "azurerm_subnet" "djsubnet" {
+  name                 = "djinternalsubnet"
+  resource_group_name  = azurerm_resource_group.djgrp.name
+  virtual_network_name = azurerm_virtual_network.djnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "djnetint" {
+  name                = "dj-nic"
+  location            = azurerm_resource_group.djgrp.location
+  resource_group_name = azurerm_resource_group.djgrp.name
+
+  ip_configuration {
+    name                          = "djinternalip"
+    subnet_id                     = azurerm_subnet.djsubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+###########
+# Data source for Key Vault
+data "azurerm_key_vault" "dj_kv" {
+  name                = "djKeyVault2"
+  resource_group_name = "dj-keyvault-rg"
+}
+
+# Data source for the secret
+data "azurerm_key_vault_secret" "admin_password" {
+  name         = "adminPassword"
+  key_vault_id = data.azurerm_key_vault.dj_kv.id
+}
+
+###########
+
+resource "azurerm_windows_virtual_machine" "djwvm" {
+  name                = "dj-machine"
+  resource_group_name = azurerm_resource_group.djgrp.name
+  location            = azurerm_resource_group.djgrp.location
+  size                = "Standard_F2"
+  # admin_username      = "azureuser"
+  # admin_password      = data.azurerm_key_vault_secret.admin_password.value
+  network_interface_ids = [
+    azurerm_network_interface.djnetint.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  admin_username = "azureuser"
+  admin_password = data.azurerm_key_vault_secret.admin_password.value
+
+
+}
